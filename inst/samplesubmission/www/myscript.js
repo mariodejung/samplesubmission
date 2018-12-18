e112131abb9b21d1f685dbf5bddc644ea0133cba
IMB30 <imb30@uni-mainz.de>
2018-12-18 09:26:36 +0100

    bugs

--- inst/samplesubmission/www/myscript.js
+++ inst/samplesubmission/www/myscript.js
@@ -1,5 +1,5 @@
-$(document).on('shiny:error', function(event) {
-  alert('Error occured');
+$(document).on('shiny:error', function(name, error, binding) {
+  alert('Error occured: ' + error);
 });
 Shiny.addCustomMessageHandler('error_occured', function(message) {
   alert(message);
