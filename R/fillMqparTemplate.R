b052796d5d363b361e79490d4ac91f5a6008a0e0
IMB30 <imb30@uni-mainz.de>
2019-01-24 15:56:46 +0100

    bugfix and improve mqpar creation function

--- R/fillMqparTemplate.R
+++ R/fillMqparTemplate.R
@@ -2,7 +2,9 @@
 #' @param paramterGroups list of parameterGroups settings (mode, enzymes)
 #' @param fastaFiles vector of fasta files paths
 #' @param rawFiles optionaly vector of raw files paths
-#' @param templateFile template File path
+#' @param templateFile template File path. If NULL(standard), attempts read template from package by means of version
+#' @param fixedFolder should MaxQuant create an extra folder.
+#' @version MaxQuant version. for each version there should be a template file in the package 
 #' @export
 #' @return filled mqpar string
 #' @examples
@@ -13,11 +15,22 @@
 fillMqparTemplate <- function (parameterGroups,
                                fastaFiles,
                                rawFiles=NULL,
-                               templateFile="mqpar.tmp.old.xml",
+                               templateFile=NULL,
                                fixedFolder='',
-                               old_version=TRUE){
+                               version="1.5.2.8"){
+  
+  if(is.null(templateFile)){
+    templateFile <- system.file('samplesubmission',
+                                paste("mqpar.tmp.", version, ".xml", sep=""),
+                                package='samplesubmission')
+  }
+  if(!file.exists(templateFile)){
+    stop("cannot find template file: \n")
+  }
   template <- readLines(templateFile)
-  parameterGroup_string <- 
+  
+  parameterGroup_string <- list()
+  parameterGroup_string['1.6.3.4'] <- 
     "<parameterGroup>
   <msInstrument>0</msInstrument>
   <maxCharge>7</maxCharge>
@@ -140,8 +153,90 @@ fillMqparTemplate <- function (parameterGroups,
   </parameterGroup>"
   
   
+  parameterGroup_string['1.5.2.8'] <-
+    "
+    <parameterGroup>
+         <maxCharge>7</maxCharge>
+  <minPeakLen>2</minPeakLen>
+  <useMs1Centroids>false</useMs1Centroids>
+  <useMs2Centroids>false</useMs2Centroids>
+  <cutPeaks>true</cutPeaks>
+  <gapScans>1</gapScans>
+  <minTime>NaN</minTime>
+  <maxTime>NaN</maxTime>
+  <matchType>MatchFromAndTo</matchType>
+  <centroidMatchTol>8</centroidMatchTol>
+  <centroidMatchTolInPpm>true</centroidMatchTolInPpm>
+  <centroidHalfWidth>35</centroidHalfWidth>
+  <centroidHalfWidthInPpm>true</centroidHalfWidthInPpm>
+  <valleyFactor>1.4</valleyFactor>
+  <advancedPeakSplitting>false</advancedPeakSplitting>
+  <intensityThreshold>500</intensityThreshold>
+  <msInstrument>0</msInstrument>
+  <intensityDetermination>0</intensityDetermination>
+  <labelMods>
+  <string />
+  </labelMods>
+  <lfqMinEdgesPerNode>3</lfqMinEdgesPerNode>
+  <lfqAvEdgesPerNode>6</lfqAvEdgesPerNode>
+  <fastLfq>true</fastLfq>
+  <lfqMinRatioCount>2</lfqMinRatioCount>
+  <useNormRatiosForHybridLfq>true</useNormRatiosForHybridLfq>
+  <maxLabeledAa>0</maxLabeledAa>
+  <maxNmods>5</maxNmods>
+  <maxMissedCleavages>2</maxMissedCleavages>
+  <multiplicity>1</multiplicity>
+<enzymeMode>%s</enzymeMode>
+  <enzymes>
+  %s
+  </enzymes>
+  <enzymesFirstSearch />
+  <useEnzymeFirstSearch>false</useEnzymeFirstSearch>
+  <useVariableModificationsFirstSearch>false</useVariableModificationsFirstSearch>
+  <variableModifications>
+  <string>Acetyl (Protein N-term)</string>
+  <string>Oxidation (M)</string>
+  </variableModifications>
+  <isobaricLabels />
+  <variableModificationsFirstSearch />
+  <hasAdditionalVariableModifications>false</hasAdditionalVariableModifications>
+  <additionalVariableModifications />
+  <additionalVariableModificationProteins />
+  <doMassFiltering>true</doMassFiltering>
+  <firstSearchTol>20</firstSearchTol>
+  <mainSearchTol>4.5</mainSearchTol>
+  <searchTolInPpm>true</searchTolInPpm>
+  <isotopeMatchTol>2</isotopeMatchTol>
+  <isotopeMatchTolInPpm>true</isotopeMatchTolInPpm>
+  <isotopeTimeCorrelation>0.6</isotopeTimeCorrelation>
+  <theorIsotopeCorrelation>0.6</theorIsotopeCorrelation>
+  <recalibrationInPpm>true</recalibrationInPpm>
+  <intensityDependentCalibration>false</intensityDependentCalibration>
+  <minScoreForCalibration>70</minScoreForCalibration>
+  <matchLibraryFile>false</matchLibraryFile>
+  <libraryFile />
+  <matchLibraryMassTolPpm>0</matchLibraryMassTolPpm>
+  <matchLibraryTimeTolMin>0</matchLibraryTimeTolMin>
+  <matchLabelTimeTolMin>0</matchLabelTimeTolMin>
+  <reporterMassTolerance>NaN</reporterMassTolerance>
+  <reporterPif>NaN</reporterPif>
+  <filterPif>false</filterPif>
+  <reporterFraction>NaN</reporterFraction>
+  <reporterBasePeakRatio>NaN</reporterBasePeakRatio>
+  <lcmsRunType>Standard</lcmsRunType>
+  <lfqMode>0</lfqMode>
+  
+  <enzymeModeFirstSearch>0</enzymeModeFirstSearch>
+  </parameterGroup>
+  "
+  
+  if(is.null(parameterGroup_string[version])){
+    stop(version, " is unknown. Valid versions: \n",
+         paste(names(parameterGroup_string), sep=", "))
+  }
+  
   par_groups <- paste(sapply(parameterGroups, function(settings) {
-    sprintf(parameterGroup_string,
+    sprintf(parameterGroup_string[[version]],
             settings['mode'],
             paste(sprintf('\n<string>%s</string>\n', 
                           switch(is.na(settings['enzyme']) + 1, 
@@ -160,6 +255,10 @@ fillMqparTemplate <- function (parameterGroups,
   patterns <- getProtIdRegexFromFileName(fastaFiles)
   patterns <- gsub("\\\\", "\\\\\\\\", patterns) # mask \\ for substitution
   fasta_paths <- preparePathForSub(fastaFiles)
+  
+  if(version < '1.6'){
+    fasta_paths <- paste0("<string>", fasta_paths, " </string>", collapse="\n")
+  }else{
   fasta_paths <- paste0("<FastaFileInfo>
                         <fastaFilePath>", fasta_paths, "</fastaFilePath>
                         <identifierParseRule>", patterns, "</identifierParseRule>
@@ -169,10 +268,8 @@ fillMqparTemplate <- function (parameterGroups,
                         <modificationParseRule></modificationParseRule>
                         <taxonomyId></taxonomyId>
                         </FastaFileInfo>", collapse="\n")
-  
-  if(old_version){
-    fasta_paths <- paste0("<string>", fasta_paths, " </string>", collapse="\n")
   }
+  
   
   template <- sub("##__Parameter_Groups__##", par_groups, template)
   template <- sub("##__Group_Indices__##", group_indices, template)
